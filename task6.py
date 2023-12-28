85b2a776fe8dfec91f6992914cf03a4c77d09324
SpideErik <erikozh@gmail.com>
2023-12-28 19:12:28 +0300

    task5

--- task6.py
+++ task6.py
@@ -1,37 +1,35 @@
 from random import randint, sample
 
-words = []
-
 
 def get_words():
-    global words
     words = []
     for i in input('Введите слова>').split():
         words.append(i.lower())
+    return words
 
 
-def print_prompt():
-    word = words[idx_word]
+def print_prompt(words, idx):
+    word = words[idx]
     na = word.count('а')
     no = word.count('о')
     print(f'Количество букв а: {na}, количество букв о: {no}')
-    print(f'Две буквы из слова слева', sample(words[idx_word-1], 2))
-    if len(word) < len(words[idx_word+1]):
+    print(f'Две буквы из слова слева', sample(words[idx-1], 2))
+    if len(word) < len(words[idx+1]):
         print('Слово справа длиннее')
     else:
         print('Слово справа короче')
 
 
-def print_words():
+def print_words(words):
     for i, v in enumerate(words, 1):
         print(f'{i}:{v}', end=' ')
     print()
 
 
-get_words()
+words = get_words()
 idx_word = randint(1, len(words) - 2)
-print_prompt()
-print_words()
+print_prompt(words, idx)
+print_words(words)
 n = int(input('Введите номер слова>')) - 1
 if idx_word == n:
     print('Угадал')
